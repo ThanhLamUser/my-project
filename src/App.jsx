cd2a32c9dd75360482be651d3184edda1bc60d1b
danglinh152 <danglinh.k4@gmail.com>
2024-11-16 03:40:33 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         {/* Route for the root path */}
         <Route
-          path="*"
+          path="/*"
           element={
             <>
               <Navbar />
