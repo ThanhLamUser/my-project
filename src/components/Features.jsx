bbcd8839f06deefbc5cbb6b810b2f1764505a43b
ThanhLamUser <vanlamhv5@gmail.com>
2024-11-16 04:28:49 +0700

    commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -20,9 +20,9 @@ const Features = () => {
                  variants={FadeIn('up',0.2)}
             initial="hidden"
             whileInView={"show"}
-            viewport={{once:false,amount: 0.7}}
+            viewport={{once:false,amount: 0.1 }}
                  className="w-full lg:w-3/4">
-                    <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-start md:gap-12 gap-8'>
+                    <div className='lg:grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-start md:gap-12 gap-8 hidden'>
                         <div className='bg-[rgba(255,255,255,0.04)] rounded-[35px] h-96 shadow-3xl p-8 items-center flex justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer '>
                             <div className='flex flex-col justify-center items-center'>
                                 <img src={feature}/>   
