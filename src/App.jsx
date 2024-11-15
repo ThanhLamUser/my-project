aad1c238c533cc7fc73914a1954e2622fe7c1590
danglinh152 <danglinh.k4@gmail.com>
2024-11-16 03:27:20 +0700

    completet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Features from "./components/Features";
@@ -9,10 +10,16 @@ import Newsletter from "./components/Newsletter";
 import Footer from "./shared/Footer";
 
 function App() {
+  useEffect(() => {
+    // Force reload page when navigating
+    if (performance.getEntriesByType("navigation")[0].type !== "reload") {
+      window.location.reload();
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
-        {/* Route for the root path */}
         <Route
           path="/"
           element={
@@ -27,7 +34,7 @@ function App() {
             </>
           }
         />
-        {/* Redirect all other paths to "/" */}
+        {/* Redirect all invalid paths to "/" */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
