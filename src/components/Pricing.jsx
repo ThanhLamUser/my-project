a6c921fca9c5580546502fd44efe1f863d63c9e3
danglinh152 <danglinh.k4@gmail.com>
2024-11-16 19:02:20 +0700

    complete routing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -76,17 +76,44 @@ const Pricing = () => {
   const [isYearly, setISYearly] = useState(false);
 
   const packages = [
-    { count: 1, name: "Start", monthlyPrice: 1000, yearlyPrice: 199, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectuer adipiscing elit.", green: rectangle18 },
-    { count: 2, name: "Advance", monthlyPrice: 2000, yearlyPrice: 399, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectuer adipiscing elit.", green: rectangle18 },
-    { count: 3, name: "Premium", monthlyPrice: 3000, yearlyPrice: 599, description: "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectuer adipiscing elit.", green: rectangle18 },
-
-  ]
+    {
+      count: 1,
+      name: "Start",
+      monthlyPrice: 1000,
+      yearlyPrice: 199,
+      description:
+        "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectuer adipiscing elit.",
+      green: rectangle18,
+    },
+    {
+      count: 2,
+      name: "Advance",
+      monthlyPrice: 2000,
+      yearlyPrice: 399,
+      description:
+        "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectuer adipiscing elit.",
+      green: rectangle18,
+    },
+    {
+      count: 3,
+      name: "Premium",
+      monthlyPrice: 3000,
+      yearlyPrice: 599,
+      description:
+        "A common form of Lorem ipsum reads: Lorem ipsum dolor sit amet, consectuer adipiscing elit.",
+      green: rectangle18,
+    },
+  ];
 
   return (
-    <div className="md:px-14 p-4 max-w-s mx-auto py-10" id="pricing" >
+    <div className="md:px-14 p-4 max-w-s mx-auto py-10" id="pricing">
       <div className="text-center">
-        <h2 className="md:text-5xl text-3xl font-extrabold text-primary mb-2 font-serif">BẢNG GIÁ CHÍNH THỨC </h2>
-        <p className="text-tartiary md:w-1/3 mx-auto px-4 font-serif">PHIÊN BẢN MỚI NHẤT 2025 - DÀNH CHO BẠN HỌC VIỆT NAM</p>
+        <h2 className="md:text-5xl text-3xl font-extrabold text-primary mb-2 font-serif">
+          BẢNG GIÁ CHÍNH THỨC{" "}
+        </h2>
+        <p className="text-tartiary md:w-1/3 mx-auto px-4 font-serif">
+          PHIÊN BẢN MỚI NHẤT 2025 - DÀNH CHO BẠN HỌC VIỆT NAM
+        </p>
       </div>
       {/* <div className="mt-16 flex justify-center">
                 <label htmlFor="toggle" className="inline-flex items-center cursor-pointer">
@@ -98,31 +125,59 @@ const Pricing = () => {
                  </label>
                  <input type="checkbox" id="toggle" className="hidden" checked={isYearly} onChange={() => setISYearly(!isYearly)} />
             </div>  */}
-      <motion.div id="pricing"
-        variants={FadeIn('up', 0.2)}
+      <motion.div
+        id="pricing"
+        variants={FadeIn("up", 0.2)}
         initial="hidden"
         whileInView={"show"}
         viewport={{ once: false, amount: 0.1 }}
-        className="grid sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-8 md:w-11/12 mx-auto">
-        {
-          packages.map((pkg, index) => <div className="border py-10 md:px-6 px-4 rounded-lg shadow-3xl" key={index}>
-            <h3 className="text-3xl font-bold text-center text-primary ">{pkg.name}</h3>
-            <p className="text-tartiary my-5 font-serif text-justify">{pkg.description}</p>
+        className="grid sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-8 md:w-11/12 mx-auto"
+      >
+        {packages.map((pkg, index) => (
+          <div
+            className="border py-10 md:px-6 px-4 rounded-lg shadow-3xl"
+            key={index}
+          >
+            <h3 className="text-3xl font-bold text-center text-primary ">
+              {pkg.name}
+            </h3>
+            <p className="text-tartiary my-5 font-serif text-justify">
+              {pkg.description}
+            </p>
             <p className="mt-5 text-center text-secondary text-4xl font-bold">
-              {isYearly ? `${pkg.yearlyPrice} VND` : `${pkg.monthlyPrice} VND`} <span className="text-base text-tartiary font-medium">/{isYearly ? 'năm' : 'tháng'}</span>
+              {isYearly ? `${pkg.yearlyPrice} VND` : `${pkg.monthlyPrice} VND`}{" "}
+              <span className="text-base text-tartiary font-medium">
+                /{isYearly ? "năm" : "tháng"}
+              </span>
             </p>
             <ul className="mt-4 space-y-2">
-              <li className="flex gap-3 flex-row items-center"><img src={pkg.green} alt="" className="w-5" /> Video of lessons</li>
-              <li className="flex gap-3 flex-row items-center"><img src={pkg.green} alt="" className="w-5" /> Homework check</li>
-              <li className="flex gap-3 flex-row items-center"><img src={pkg.green} alt="" className="w-5" /> Additional practical task</li>
-              <li className="flex gap-3 flex-row items-center"><img src={pkg.green} alt="" className="w-5" /> Monthly conferences</li>
-              <li className="flex gap-3 flex-row items-center"><img src={pkg.green} alt="" className="w-5" /> Personal advice from teachers</li>
+              <li className="flex gap-3 flex-row items-center">
+                <img src={pkg.green} alt="" className="w-5" /> Video of lessons
+              </li>
+              <li className="flex gap-3 flex-row items-center">
+                <img src={pkg.green} alt="" className="w-5" /> Homework check
+              </li>
+              <li className="flex gap-3 flex-row items-center">
+                <img src={pkg.green} alt="" className="w-5" /> Additional
+                practical task
+              </li>
+              <li className="flex gap-3 flex-row items-center">
+                <img src={pkg.green} alt="" className="w-5" /> Monthly
+                conferences
+              </li>
+              <li className="flex gap-3 flex-row items-center">
+                <img src={pkg.green} alt="" className="w-5" /> Personal advice
+                from teachers
+              </li>
             </ul>
-            <div className="w-full mx-auto mt-8 flex items-center justify-center">
-              <button className="btnPrimary" onClick={() => checkOut(pkg.count)}>MUA NGAY</button>
+            <div
+              className="w-full mx-auto mt-8 flex items-center justify-center"
+              onClick={() => checkOut(pkg.count)}
+            >
+              <button className="btnPrimary">MUA NGAY</button>
             </div>
-          </div>)
-        }
+          </div>
+        ))}
       </motion.div>
     </div>
   );
